36d5efb9394a41c0f8ea4025763b4a2bc39a81a5
Nazish Ajaz <nazishajazkhan@gmail.com>
2026-06-28 21:06:37 +0500

    Implement user registration with password hashing

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 
 const connectDB = require("./config/db");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
 
 connectDB();
@@ -10,6 +12,7 @@ connectDB();
 app.use(express.json());
 
 app.use("/", require("./routes"));
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 

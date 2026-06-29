02edd7b8c4e7e6c2705f4b895270d153c1660268
Nazish Ajaz <nazishajazkhan@gmail.com>
2026-06-29 08:00:02 +0500

    Implement blog post update

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -18,3 +18,75 @@ exports.createPost = async (req, res) => {
         });
     }
 };
+
+exports.getPosts = async (req, res) => {
+    try {
+        const posts = await Post.find().populate("author", "name email");
+
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error",
+            error: error.message,
+        });
+    }
+};
+
+exports.getPostById = async (req, res) => {
+    try {
+
+        console.log("Requested ID:", req.params.id);
+
+        const post = await Post.findById(req.params.id).populate(
+            "author",
+            "name email"
+        );
+
+        if (!post) {
+            return res.status(404).json({
+                message: "Post not found",
+            });
+        }
+
+        res.status(200).json(post);
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error",
+            error: error.message,
+        });
+    }
+};
+
+exports.updatePost = async (req, res) => {
+    try {
+        console.log("Request body:", req.body);
+        const { title, content } = req.body;
+
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({
+                message: "Post not found",
+            });
+        }
+
+        if (post.author.toString() !== req.user.id) {
+            return res.status(403).json({
+                message: "Not authorized",
+            });
+        }
+
+        post.title = title || post.title;
+        post.content = content || post.content;
+
+        await post.save();
+
+        res.status(200).json(post);
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Server error",
+            error: error.message,
+        });
+    }
+};
--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -2,8 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 const protect = require("../middleware/authMiddleware");
-const { createPost } = require("../controllers/postController");
+
+const {
+    createPost,
+    getPosts,
+    getPostById,
+    updatePost,
+} = require("../controllers/postController");
+
+router.get("/", getPosts);
+
+router.get("/:id", getPostById);
 
 router.post("/", protect, createPost);
+
+router.put("/:id", protect, updatePost);
 
 module.exports = router;
